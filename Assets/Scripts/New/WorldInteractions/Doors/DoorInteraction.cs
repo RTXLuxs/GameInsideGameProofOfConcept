7d481c3b3d5d0b151653d88683916ea2ad33d46b
RTXLuxs <166643366+RTXLuxs@users.noreply.github.com>
2026-07-05 16:11:12 +0200

    Item Requirement Part 2
    [Doors]
    - Updated door script to now use InteractionRequirements.cs
    	- If no requirements are present, door remains accessible
    	- With requirement, door will only be usable with the corresponding requirement fulfilled

--- Assets/Scripts/New/WorldInteractions/Doors/DoorInteraction.cs
+++ Assets/Scripts/New/WorldInteractions/Doors/DoorInteraction.cs
@@ -3,34 +3,50 @@ using UnityEngine;
 public class DoorInteraction : MonoBehaviour, IInteractable
 {
     private WorldObject thisObject;
+    private DoorState doorState;
+    private InteractionRequirements requirements;
 
     private void Awake()
     {
         thisObject = GetComponent<WorldObject>();
+        doorState = GetComponent<DoorState>();
+        requirements = GetComponent<InteractionRequirements>();
     }
 
     public string GetInteractionText()
     {
         var state = thisObject.GetState();
 
-        return state == WorldObjectState.Open? "Close DOOR [E]": "Open DOOR [E]";
+        return state == WorldObjectState.Open
+            ? "Close DOOR [E]"
+            : "Open DOOR [E]";
     }
 
     public void Interact()
     {
         var currentState = thisObject.GetState();
 
-        if (currentState == WorldObjectState.Open) //Door is currently open
+        // Door is currently open -> Always allow closing
+        if (currentState == WorldObjectState.Open)
         {
             thisObject.SetState(WorldObjectState.Closed);
-            thisObject.GetComponent<DoorState>().CloseDoor();
+            doorState.CloseDoor();
+
             Debug.Log($"[{thisObject.objectId}] CLOSED");
+            return;
         }
-        else //Door is currently closed
+
+        // Door is currently closed -> Check requirements before opening
+        if (requirements != null &&
+            !requirements.AreSatisfied(out string failureReason))
         {
-            thisObject.SetState(WorldObjectState.Open);
-            thisObject.GetComponent<DoorState>().OpenDoor();
-            Debug.Log($"[{thisObject.objectId}] OPEN");
+            Debug.Log(failureReason);
+            return;
         }
+
+        thisObject.SetState(WorldObjectState.Open);
+        doorState.OpenDoor();
+
+        Debug.Log($"[{thisObject.objectId}] OPEN");
     }
 }
